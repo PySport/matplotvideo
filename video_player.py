8ef8b026f857c21bde8d596f99c1fab05c0c7700
Koen Vossen <koen@teamtv.nl>
2020-05-06 23:18:50 +0200

    More changes

--- video_player.py
+++ video_player.py
@@ -18,6 +18,7 @@ class FrameChange(Enum):
 
 
 class CV2VideoPlayer:
+    KEY_CODE_STOP = 27
     KEY_CODE_TOGGLE_PLAY = 32
     KEY_CODE_PREV_FRAME = ord('a')
     KEY_CODE_NEXT_FRAME = ord('d')
@@ -29,9 +30,11 @@ class CV2VideoPlayer:
     __frame_count: int
     __fps: int
     __on_frame_callback: Callable
+    __on_stop_callback: Callable
     __previous_frame_display_timestamp: float
+    __window_name: str = 'VideoPlayer'
 
-    def __init__(self, filename: str, on_frame: Callable = None):
+    def __init__(self, filename: str, on_frame: Callable, on_stop: Callable):
         self.__cap = cv2.VideoCapture(filename)
 
         self.__frame_count = int(self.__cap.get(cv2.cv2.CAP_PROP_FRAME_COUNT))
@@ -41,6 +44,7 @@ class CV2VideoPlayer:
 
         self.__current_frame = 0
         self.__on_frame_callback = on_frame
+        self.__on_stop_callback = on_stop
         self.__previous_frame_display_timestamp = 0
 
         self.__status = 'play'
@@ -54,19 +58,19 @@ class CV2VideoPlayer:
                 self.__status = 'seek_frame'
 
         def on_change_playback_rate(x):
+            if x == 0:
+                x = 1
             self.__playback_rate = x / 100.
 
-        cv2.namedWindow('image')
-        cv2.createTrackbar('Frame', 'image', 0, self.__frame_count - 1, on_change_frame)
-        cv2.setTrackbarPos('Frame', 'image', 0)
+        cv2.namedWindow(self.__window_name)
+        cv2.createTrackbar('Frame', self.__window_name, 0, self.__frame_count - 1, on_change_frame)
+        cv2.setTrackbarPos('Frame', self.__window_name, 0)
 
-        cv2.createTrackbar('Playback Speed', 'image', 0, 400, on_change_playback_rate)
-        cv2.setTrackbarPos('Playback Speed', 'image', int(self.__playback_rate * 100))
+        cv2.createTrackbar('Playback Speed', self.__window_name, 1, 400, on_change_playback_rate)
+        cv2.setTrackbarPos('Playback Speed', self.__window_name, int(self.__playback_rate * 100))
 
     def __handle_keyboard_input(self):
         key = cv2.waitKey(1)
-        if key != -1:
-            print("key",key)
         if key == self.KEY_CODE_TOGGLE_PLAY:
             if self.__status == 'paused':
                 self.__status = 'play'
@@ -76,10 +80,10 @@ class CV2VideoPlayer:
             self.__status = 'prev_frame'
         elif key == self.KEY_CODE_NEXT_FRAME:
             self.__status = 'next_frame'
+        elif key == self.KEY_CODE_STOP:
+            self.stop()
 
     def __calculate_current_frame(self) -> FrameChange:
-        if self.__status != 'paused':
-            print(self.__status)
         if self.__status == 'play':
             if self.__current_frame == self.__frame_count:
                 self.__status = 'paused'
@@ -94,7 +98,6 @@ class CV2VideoPlayer:
             time_delta = now - self.__previous_frame_display_timestamp
 
             frame_delta = int(time_delta / frame_display_time)
-            print(time_delta, frame_display_time, frame_delta)
 
             if frame_delta > 0:
                 self.__current_frame += frame_delta
@@ -126,9 +129,12 @@ class CV2VideoPlayer:
         r = 750.0 / im.shape[1]
         dim = (750, int(im.shape[0] * r))
         im = cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
-        cv2.imshow('image', im)
+        cv2.imshow(self.__window_name, im)
 
     def on_timer(self):
+        if self.__status == "stopped":
+            return
+
         self.__handle_keyboard_input()
         frame_change = self.__calculate_current_frame()
 
@@ -137,27 +143,27 @@ class CV2VideoPlayer:
         elif frame_change == FrameChange.Next:
             pass  # by default capture reads next frame
         elif frame_change == FrameChange.Seek:
-            print(self.__current_frame)
             self.__cap.set(cv2.cv2.CAP_PROP_POS_FRAMES, self.__current_frame)
 
         self.__show_current_frame()
-
+        cv2.setTrackbarPos('Frame', self.__window_name, self.__current_frame)
         self.__on_frame_callback(self.__current_frame)
 
     def stop(self):
         del self.__cap
-        cv2.destroyWindow('image')
+        cv2.destroyWindow(self.__window_name)
+        self.__status = "stopped"
+        self.__on_stop_callback()
 
 
 def attach_video_player(figure: Figure, filename: str, on_frame: Callable = None):
-    video_player = CV2VideoPlayer(filename, on_frame)
+    timer = None
+
+    def on_stop():
+        timer.stop()
+
+    video_player = CV2VideoPlayer(filename, on_frame, on_stop)
 
     timer = figure.canvas.new_timer(interval=1,
                                     callbacks=[(video_player.on_timer, [], {})])
     timer.start()
-
-    def _stop(e):
-        timer.stop()
-        video_player.stop()
-
-    figure.canvas.mpl_connect('close_event', _stop)
